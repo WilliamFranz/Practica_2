667eca91c5e34c30592f98a697235b24af6a9af4
WilliamFranz <franzfernanddez@gmail.com>
2024-06-25 09:49:07 -0600

    Update ejercicio5.java

--- src/main/java/com/emergentes/ejercicio5.java
+++ src/main/java/com/emergentes/ejercicio5.java
@@ -9,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Eva Carmen Huaylliri Ajata
- */
+
 @WebServlet(name = "ejercicio5", urlPatterns = {"/ejercicio5"})
 public class ejercicio5 extends HttpServlet {
 
