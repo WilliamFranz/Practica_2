6612a7ad4626b8ee8d11022b1ff312042f3644d4
WilliamFranz <franzfernanddez@gmail.com>
2024-06-25 09:47:59 -0600

    Update ejercicio2.java

--- src/main/java/com/emergentes/ejercicio2.java
+++ src/main/java/com/emergentes/ejercicio2.java
@@ -9,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Eva Carmen Huaylliri Ajata
- */
+
 @WebServlet(name = "ejercicio2", urlPatterns = {"/ejercicio2"})
 public class ejercicio2 extends HttpServlet {
     protected void processRequest(HttpServletRequest request,
